4ac74e26246c862a34a1c830cff8d7fd03e06d4d
sinequanon <dleck28@gmail.com>
2023-12-02 16:15:51 +0900

    quit clear test2

--- vite.c
+++ vite.c
@@ -1005,13 +1005,14 @@ void shortcut_key(void){
                 // sprintf(buf, "quit status : %d", quit_status);
                 // write(STDOUT_FILENO, buf, strlen(buf));
                 tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios_quit);
+                system("clear");
                 write(STDOUT_FILENO, "\033[H", strlen("\033[H"));
-                for(int i = 0; i< terminal_row_size; ++i){
-                    char buf[30];
-                    sprintf(buf,"\033[%d;%dH", i, 0);
-                    write(STDOUT_FILENO, "\033[K", strlen("\033[K"));
-                }
-                write(STDOUT_FILENO, "\033[H", strlen("\033[H"));
+                // for(int i = 0; i< terminal_row_size; ++i){
+                //     char buf[30];
+                //     sprintf(buf,"\033[%d;%dH", i, 0);
+                //     write(STDOUT_FILENO, "\033[K", strlen("\033[K"));
+                // }
+                // write(STDOUT_FILENO, "\033[H", strlen("\033[H"));
                 exit(0);
             }else{
                 //print message bar
