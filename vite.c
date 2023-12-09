84a094e75de0e5420c002ad97ab5485167a29f3c
SineQuaNon <dleck28@gmail.com>
2023-12-09 16:24:19 +0900

    123

--- vite.c
+++ vite.c
@@ -1074,10 +1074,11 @@ void enter_process(void){
             
             char buf2[100];
             memmove(&row_info[cursor_y+cursor_y_out+2], &(row_info[cursor_y+cursor_y_out+1]), sizeof(file_row_info) *  (file_row_length - (cursor_y+cursor_y_out)-1));
+            char * new_line  = (char*)malloc(sizeof(char) * 1);
             row_info[cursor_y+cursor_y_out+1].row = new_line();
             row_info[cursor_y+cursor_y_out+1].row = realloc(row_info[cursor_y+cursor_y_out+1].row, sizeof(char));
             row_info[cursor_y+cursor_y_out+1].len = 1;
-            sprintf(buf2, "row : %s , size : %ld", row_info[cursor_y+cursor_y_out+1].row, sizeof(char));
+            sprintf(buf2, "row : %s , size : %ld", new_line, sizeof(char));
             write(STDOUT_FILENO, buf2, strlen(buf2));
             file_row_length++;
             //input_file_line();
